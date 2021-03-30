a0563e733ef402f772102e6e8f9f02ad3b0eb2ae
lenglongleng <chalermchai.sawettikamporn@krungthai.com>
2021-03-30 15:35:23 +0700

    first commit

---
+++ src/main/java/com/repo/SpringbootTestRepo21Application.java
@@ -0,0 +1,13 @@
+package com.repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestRepo21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestRepo21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/repo/SpringbootTestRepo21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestRepo21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
